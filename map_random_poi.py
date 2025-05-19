8d6267ce1f785e40282275be667ca30fae3a9155
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2025-05-19 00:09:02 -0400

    Documentation

--- map_random_poi.py
+++ map_random_poi.py
@@ -11,6 +11,7 @@ import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Using the overpass API for mapping
 OVERPASS_URL = "http://overpass-api.de/api/interpreter"
 os.environ["OMP_NUM_THREADS"] = "1"
 warnings.filterwarnings(
@@ -19,6 +20,7 @@ warnings.filterwarnings(
     category=UserWarning
 )
 
+# Generate coordinates from an address
 def GetCoordinates(address):
     geolocator = Nominatim(user_agent="geoapi")
     location = geolocator.geocode(address)
@@ -27,7 +29,10 @@ def GetCoordinates(address):
     else:
         return None
 
+# Using coordinates, find POIs using a query
 def FindPOIs(lat, lon, rad):
+
+    # Query for amenity, tourism, leisure, and shop tags
     query = f"""
     [out:json];
     (
@@ -48,9 +53,12 @@ def FindPOIs(lat, lon, rad):
     );
     out center tags;
     """
+
     response = requests.get(OVERPASS_URL, params={'data': query})
     data = response.json()
     pois = []
+
+    # Parse the JSON file to put POIs in a list
     for el in data['elements']:
         name = el.get('tags', {}).get('name', 'Unnamed POI')
         lat = el.get('lat')
@@ -68,7 +76,10 @@ def FindPOIs(lat, lon, rad):
 
     return pois
 
+# Get coordinates from a POI's text name
 def ParsePOI(poi):
+
+    # Check for a text name match or a coordinates match using regex
     try:
         name_match = re.match(r"^(.*?) at", poi)
         coords_match = re.search(r"\(([-\d.]+), ([-\d.]+)\)", poi)
@@ -84,10 +95,12 @@ def ParsePOI(poi):
         print("Failed to parse:", poi)
         return "Unknown", "0", "0"
 
+# Create the html map of the chosen location and the found POIs
 def CreateMap(lat, lon, rad, pois, poi_counter, noise):
     Map = folium.Map(location=[lat, lon], zoom_start=13)
     folium.Marker([lat, lon], popup="Selected Location").add_to(Map)
 
+    # Radius chosen by user
     Circle(
         location=(lat, lon),
         radius=rad * 1000,
@@ -97,16 +110,12 @@ def CreateMap(lat, lon, rad, pois, poi_counter, noise):
     ).add_to(Map)
 
     offset_points = []
+
+    # Take all the found POIs and apply noise to them to generate the point
     for poi in pois:
         name, latStr, lonStr = ParsePOI(poi)
         latF = float(latStr)
         lonF = float(lonStr)
-
-        # folium.Marker(
-        #     location=[latF, lonF],
-        #     popup=f"{name} (chosen {poi_counter[poi]}x)",
-        #     icon=folium.Icon(color='green', icon='ok-sign')
-        # ).add_to(Map)
 
         #NOISE = 0.002
         for x in range(poi_counter[poi]):
@@ -124,11 +133,13 @@ def CreateMap(lat, lon, rad, pois, poi_counter, noise):
     Map.save("POI_Map.html")
     webbrowser.open('file://' + os.path.realpath("POI_Map.html"))
 
+    # Write to a text file all the found POIs
     with open("POIs.txt", "w", encoding="utf-8") as file:
         file.write("Random POI's with noise: (lat, lon): \n")
         for offset_lat, offset_lon in offset_points:
             file.write(f"({offset_lat}, {offset_lon})\n")
 
+    # Write to a text file all the POIs that were selected and how many times they were
     with open("Chosen_POIs.txt", "w", encoding="utf-8") as file:
         sorted_pois = sorted(poi_counter.items(), key=lambda x: x[1], reverse=True)
         for poi, count in sorted_pois:
@@ -136,6 +147,8 @@ def CreateMap(lat, lon, rad, pois, poi_counter, noise):
                 name, lat_str, lon_str = ParsePOI(poi)
                 file.write(f"{name} ({lat_str}, {lon_str})\n")
 
+# Harversine formula for finding the distance between two coordinates
+# https://www.geeksforgeeks.org/haversine-formula-to-find-distance-between-two-points-on-a-sphere/
 def CalculateDistance(lat1, lon1, lat2, lon2):
     R = 6371.0
     lat1_rad = np.radians(lat1)
@@ -153,8 +166,8 @@ def CalculateDistance(lat1, lon1, lat2, lon2):
     return distance    
 
 def Main():
+    # From the user get an address or coordinates for their chosen location
     ch = input("Type 'Address' or 'Coordinates': ").strip().lower()
-
     if ch == 'address':
         address = input("Enter address: ")
         coords = GetCoordinates(address)
@@ -172,18 +185,21 @@ def Main():
     else:
         print("Invalid Entry")
         return
-
+    
+    # From the user get the radius of the circle
     try:
         radius = float(input("Radius (KM): "))
     except ValueError:
         print("Invalid Radius")
         return
     
+    # From the user get the amount of noise
     try:
-        noise = float(input("Enter amount of noise (KM): "))
+        noise = float(input("Enter amount of noise (KM) (0.002 is a recommended value): "))
     except ValueError:
         print("Invalid Noise Value")
         return
+    
     
     try:
         num_runs = int(input("Enter number of runs: "))
